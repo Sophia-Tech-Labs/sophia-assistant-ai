e34407c8ecbfcf8fe0b7e67c501b184278809c1d
ayanokojix23 <ayanokojix2306@gmail.com>
2025-07-03 04:39:53 +0000

    finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const geminiAIReply = require("./lib/gemini")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,15 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Gemini route
-app.post("/", async (req, res) => {
-  try {
-
-    
-  } catch (err) {
-    console.error("Gemini error:", err);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
+app.post("/ai/reply",geminiAIReply);
 
 app.listen(PORT, () => {
   console.log(`Sophia AI server running on port ${PORT}`);
